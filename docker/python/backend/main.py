792ddb2921596c0c81383ccceef8660325f0445f
242darkman <53907600+242darkman@users.noreply.github.com>
2023-12-06 12:09:47 +0100

    feat: initialisation d'une base de données avec l'orm sqlalchemy

--- docker/python/backend/main.py
+++ docker/python/backend/main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy import create_engine, MetaData, Table, select, update
+from sqlalchemy.orm import sessionmaker, Session
 import uvicorn
 
 app = FastAPI()
 
+DATABASE_URL = "mysql://user:password@localhost/iterator-db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+metadata = MetaData()
+iterator = Table('iterator', metadata, autoload_with=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
@@ -17,9 +33,19 @@ async def root():
     """
     return {"message": "Hello World"}
 
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
+    """
+    An asynchronous function that takes a string parameter `name` and returns a dictionary with a single key-value pair. 
+
+    Parameters:
+        name (str): The name to be included in the returned message.
+
+    Returns:
+        dict: A dictionary with the key "message" and the value "Hello {name}", where {name} is the input parameter.
+    """
     return {"message": f"Hello {name}"}
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
