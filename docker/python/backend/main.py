3ec997036c941fe7b9645976104c42f8659988b0
242darkman <53907600+242darkman@users.noreply.github.com>
2023-12-06 11:55:24 +0100

    feat: ajout du server web `uvicorn`

--- docker/python/backend/main.py
+++ docker/python/backend/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    """
+    This function is the root endpoint for the API.
+    
+    Parameters:
+        None
+    
+    Returns:
+        dict: A dictionary containing the message "Hello World"
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
