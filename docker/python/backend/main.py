d6b00ea30ca93545a0853a978695ee1a9bb6deb1
242darkman <53907600+242darkman@users.noreply.github.com>
2024-02-13 09:36:54 +0100

    fix: correction des bugs du backend avec le mysql

--- docker/python/backend/main.py
+++ docker/python/backend/main.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import urllib.parse
+import json
+import mysql.connector
 import configparser
 
 config = configparser.ConfigParser()
@@ -8,32 +9,65 @@ config.read('properties/config.properties')
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        parsed_path = urllib.parse.urlparse(self.path)
-        path_parts = parsed_path.path.strip("/").split("/")
-
-        if self.path == '/':
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(b'Hello World')
-        elif self.path.startswith('/hello/'):
-            if len(path_parts) == 2:
-                name = path_parts[1]
-                message = f"Hello {name}".encode()
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write(message)
-        elif self.path == '/health':
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(b'{"status": "OK"}')
+        if self.path == '/health/' or self.path == "/health":
+            self.handle_health()
+        elif self.path == '/get/' or self.path == '/get':
+            self.handle_get()
         else:
-            self.send_error(404, "File not found")
+            self.send_error(404)
+
+    def do_POST(self):
+        if self.path == '/add/' or self.path == '/add':
+            self.handle_add()
+        else:
+            self.send_error(404)
+
+    def handle_health(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        health_check = {"status": "OK"}
+        self.wfile.write(json.dumps(health_check).encode('utf8'))
+
+    def handle_get(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        sql_connector = mysql.connector.connect(user=config['DEFAULT']['DB_USER'], password=config['DEFAULT']['DB_PASSWORD'], host='mariadb', database=config['DEFAULT']['DB_NAME'])
+        cursor = sql_connector.cursor()
+        query = ("SELECT * FROM interator")
+        cursor.execute(query)
+        iterators = []
+        for (state) in cursor:
+            entry = {"state": state}
+            iterators.append(entry)
+        response = {"status": "ok", "iterators": iterators}
+        self.wfile.write(json.dumps(response).encode('utf8'))
+        cursor.close()
+        sql_connector.close()
+
+    def handle_add(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = json.loads(post_data)
+        value = data['value']
+        sql_connector = mysql.connector.connect(user='userIterator', password='qwerty1234', host='mariadb', database='iterator-db')
+        cursor = sql_connector.cursor()
+        query = ("INSERT INTO interator (state) VALUES (%s)")
+        cursor.execute(query, (value,))
+        sql_connector.commit()
+        response = {"status": "ok", "message": "Iterator value added successfully"}
+        self.wfile.write(json.dumps(response).encode('utf8'))
+        cursor.close()
+        sql_connector.close()
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
     server_address = (config['DEFAULT']['APP_HOST'], int(config['DEFAULT']['PORT']))
     httpd = server_class(server_address, handler_class)
-    print(f'Starting httpd on {server_address[0]}:{server_address[1]}...')
     httpd.serve_forever()
-
+    
 if __name__ == '__main__':
     run()
